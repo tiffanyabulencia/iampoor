89cc74121c84e35ff4e95983c5f2372937066851
unknown <tiffanyabulencia199@gmail.com>
2023-12-21 13:34:39 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 14, 96, 143),
+      backgroundColor: Color.fromARGB(255, 207, 198, 144),
       appBar: AppBar(
-        title: Center(child: const Text('I Am Homeless')),
+        title: Center(child: const Text('I Am Poor')),
         backgroundColor: Colors.blueGrey[900],
       ),
       body: const Center(
         child: Center(
           child: Image(
-            image: AssetImage('images/poor.png'),
+            image: AssetImage('images/rich.png'),
           ),
         ),
       ),
